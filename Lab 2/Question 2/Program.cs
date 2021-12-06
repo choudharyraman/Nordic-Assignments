220a88a86195fb6a42c0808e287dc541fbd64356
Raman Choudhary <62947349+choudharyraman@users.noreply.github.com>
2021-12-06 14:42:49 +0530

    Add files via upload
    Solution of LAB Assignments

---
+++ Lab 2/Question 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the char : ");
+            char value = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine((int)value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 2/Question 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter the age in string : ");
+            string str_age = Console.ReadLine();
+            Type ty = str_age.GetType();
+            Console.WriteLine("Data type of Age is " + ty);
+            bool int_age = int.TryParse(str_age, out age);
+            Console.WriteLine("Your int value of age is " + age);
+            Type ty1 = age.GetType();
+            Console.WriteLine("After conver Data type of Age is " + ty1);
+            Console.ReadKey();
+        }
+    }
+}
