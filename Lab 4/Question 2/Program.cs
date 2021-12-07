cff66b473d6ce11ecc6cf05c64875e0f3dfc8557
Raman Choudhary <62947349+choudharyraman@users.noreply.github.com>
2021-12-07 17:36:16 +0530

    Add files via upload

---
+++ Lab 4/Question 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i = 1;
+			do
+			{
+		     
+				Console.WriteLine(i);
+				i++;
+				if (i % 2 != 0)
+				break;
+
+			} while (i <= 50);
+			Console.ReadKey();
+		}
+    }
+}
---
+++ Lab 4/Question 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
