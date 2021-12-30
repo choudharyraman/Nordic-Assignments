71f13dcc76b840ce0ed1ff0047e526441fb350c2
Raman Choudhary <62947349+choudharyraman@users.noreply.github.com>
2021-12-30 12:08:26 +0530

    Add files via upload

---
+++ Lab 8/Question 1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Computer comp = new Computer();
+            comp.BootUp();
+            comp.ShutDown();
+            Console.ReadLine();
+        }
+    }
+
+
+    public abstract class Computer
+    {
+        public void BootUp()
+        {
+            Console.WriteLine( "System is Booting");
+        }
+        public void ShutDown()
+        {
+            Console.WriteLine("System is Shuting Down");
+        }
+    }
+
+    class SuperComputer : Computer
+    {
+        public static void Computer()
+        {
+            Console.Write("Pres 1 TO BOOT AND 2 TO ShutDown. Enter Your Choice - ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            SuperComputer ob = new SuperComputer();
+            if (n == 1)
+                ob.BootUp();
+            else if (n == 2)
+                ob.ShutDown();
+            Console.ReadLine();
+        }
+    }
+
+    class MainframeComputer : Computer
+    {
+        static void Main()
+        {
+            Console.Write("Pres 1 TO BOOT AND 2 TO ShutDown. Enter Your Choice - ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            MainframeComputer ob = new MainframeComputer();
+            if (n == 1)
+                ob.BootUp();
+            else if (n == 2)
+                ob.ShutDown();
+            Console.ReadLine();
+        }
+    }
+
+    class MicroComputer : Computer
+    {
+        static void Main()
+        {
+            Console.Write("Pres 1 TO BOOT AND 2 TO ShutDown. Enter Your Choice -  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            MicroComputer ob = new MicroComputer();
+            if (n == 1)
+                ob.BootUp();
+            else if (n == 2)
+                ob.ShutDown();
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ Lab 8/Question 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Pen pen = new Pen();
+			pen.StartWriting();
+			pen.StopWriting();
+
+			Console.ReadLine();
+		}
+    }
+}
+
+sealed class Pen
+{
+	public void StartWriting()
+	{
+		Console.WriteLine("Start writing");
+	}
+	public void StopWriting()
+	{
+		Console.WriteLine("Stop writing");
+	}
+}
